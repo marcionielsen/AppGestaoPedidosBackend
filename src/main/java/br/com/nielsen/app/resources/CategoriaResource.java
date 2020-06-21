65abf47a4a2b01c24efa60dcf5301f9297a67fd8
Marcio Nielsen Baptista <marcio.nielsen@gmail.com>
2020-06-21 05:14:42 -0300

    #secao-modelo-conceitual
    Criando a classe de dominio Categoria.java;
    Teste do resource "/categorias";
    Configuração do Toolchain no pom.xml.

--- src/main/java/br/com/nielsen/app/resources/CategoriaResource.java
+++ src/main/java/br/com/nielsen/app/resources/CategoriaResource.java
@@ -1,16 +1,34 @@
 package br.com.nielsen.app.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.nielsen.app.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@GetMapping()
-	public String listar() {
-		return "Rest está funcionando!!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1L, "Lanches");
+		Categoria cat2 = new Categoria(2L, "Pizzas");
+		Categoria cat3 = new Categoria(3L, "Refrigerantes");
+		Categoria cat4 = new Categoria(4L, "Porções");
+		
+		List<Categoria> retorno = new ArrayList<>();
+		
+		retorno.add(cat1);
+		retorno.add(cat2);
+		retorno.add(cat3);
+		retorno.add(cat4);	
+				
+		return retorno;
 	}
 	
 }
