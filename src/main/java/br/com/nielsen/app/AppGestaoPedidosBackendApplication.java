0672a08c3ee16c90d3712733e2097c6bb0189c89
Marcio Nielsen Baptista <marcio.nielsen@gmail.com>
2020-06-21 01:46:52 -0300

    Criação do Projeto.

---
+++ src/main/java/br/com/nielsen/app/AppGestaoPedidosBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.nielsen.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGestaoPedidosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGestaoPedidosBackendApplication.class, args);
+	}
+
+}
